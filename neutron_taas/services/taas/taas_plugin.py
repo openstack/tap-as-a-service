ca6bd57723bc9467edfa09df411787705a4acba4
Slawek Kaplonski <skaplons@redhat.com>
2026-02-25 15:14:45 +0100

    Filter service providers configuration by the service name
    While loading service providers and their configuration from the Neutron
    config files, taas service plugins didn't filter service providers based
    on the service type thus it could load also providers of completely
    different services, like e.g. FIREWALL_V2 into the TAAS service
    providers configurations.
    
    Now it filters service providers from config files based on the service
    name so that it will not load anything not really related to the TAAS
    service.
    
    Closes-bug: #2142671
    
    Change-Id: I4397223d8747a8056ecb1d1fd495edd4e6da8960
    Signed-off-by: Slawek Kaplonski <skaplons@redhat.com>

--- neutron_taas/services/taas/taas_plugin.py
+++ neutron_taas/services/taas/taas_plugin.py
@@ -39,7 +39,7 @@ LOG = logging.getLogger(__name__)
 def add_provider_configuration(type_manager, service_type):
     type_manager.add_provider_configuration(
         service_type,
-        pconf.ProviderConfiguration('neutron_taas'))
+        pconf.ProviderConfiguration('neutron_taas', service_type))
 
 
 @registry.has_registry_receivers
